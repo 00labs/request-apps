aad951a4a6a3c9c774d7c7bdab076f14f1b35bbb
shan <shan@57blocks.com>
2023-02-24 15:26:58 +0800

    point goerli to huma node

--- packages/shared/src/helpers/client.ts
+++ packages/shared/src/helpers/client.ts
@@ -11,6 +11,13 @@ export const useRequestClient = (
   return getRequestClient(network, signatureProvider, currencyList);
 };
 
+const getBaseUrl = (networkName) => {
+  if (networkName === "goerli") {
+    return `https://${networkName}.v2.rn.huma.finance/`;
+  }
+  return `https://${networkName}.gateway.request.network/`;
+};
+
 export const getRequestClient = (
   network: string,
   signatureProvider?: Types.SignatureProvider.ISignatureProvider,
@@ -25,8 +32,9 @@ export const getRequestClient = (
   const networkName = networkMap[network] ?? network;
   const requestNetwork = new RequestNetwork({
     nodeConnectionConfig: {
+      baseURL: getBaseUrl(networkName),
       // baseURL: `https://${networkName}.gateway.request.network/`,
-      baseURL: `https://${networkName}.v2.rn.huma.finance/`,
+      // baseURL: `https://${networkName}.v2.rn.huma.finance/`,
       // baseURL: `http://localhost:3000`,
     },
     signatureProvider,
